9959020b7a9eba64913b975f4129bd10547f31bf
Helen Oliveira <sousahelen816@gmail.com>
2022-08-12 20:04:15 -0300

    generics

---
+++ teste.js
@@ -0,0 +1,7 @@
+//arquivo de prod
+
+
+function cont(x, y) {
+    return x * y;
+}
+console.log(cont(2, 4));
---
+++ teste.ts
@@ -0,0 +1,11 @@
+//arquivo de desenvolvimento
+
+function cont(x:number,y:number){
+    return x * y;
+}
+
+console.log(cont(2 ,4))
+
+
+
+export default cont; //exportar para se tornar um arquivo unico
